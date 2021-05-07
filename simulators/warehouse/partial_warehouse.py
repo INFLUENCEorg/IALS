e9ab694878cc39aa6e0ee64664b65d129cbdfdef
miguelsuau <miguel.suau@gmail.com>
2021-05-07 16:34:49 +0200

    dset=obs

--- simulators/warehouse/partial_warehouse.py
+++ simulators/warehouse/partial_warehouse.py
@@ -161,8 +161,8 @@ class PartialWarehouse(object):
         state = self._get_state()
         robot = self.robots[self.learning_robot_id]
         obs = robot.observe(state, 'vector')
-        dset = obs[49:]
-        # dset = obs
+        # dset = obs[49:]
+        dset = obs
         return dset
         
     ######################### Private Functions ###########################
