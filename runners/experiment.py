654d27949471483b500579befb2db1d0df29e02b
miguelsuau <miguel.suau@gmail.com>
2021-07-12 20:55:30 +0200

    no output hidden memory

--- runners/experiment.py
+++ runners/experiment.py
@@ -210,8 +210,9 @@ class Experiment(object):
             # NOTE: Episodes in all envs must terminate at the same time 
             while not done[0]:
                 n_steps += 1
-                action, _, _, _ = self.agent.choose_action(obs)
+                action, _, _= self.agent.choose_action(obs)
                 obs, reward, done, info = self.global_env.step(action)
+                self.agent.reset_hidden_memory(done)
                 dset.append(np.array([i['dset'] for i in info]))
                 infs.append(np.array([i['infs'] for i in info]))
                 reward_sum += reward
@@ -232,8 +233,9 @@ class Experiment(object):
             # NOTE: Episodes in all envs must terminate at the same time
             while not done[0]:
                 n_steps += 1
-                action, _, _, _ = self.agent.choose_action(obs)
+                action, _, _ = self.agent.choose_action(obs)
                 obs, reward, done, _ = self.global_env.step(action)
+                self.agent.reset_hidden_memory(done)
                 reward_sum += reward
             episode_rewards.append(reward_sum)
         print('Done!')
